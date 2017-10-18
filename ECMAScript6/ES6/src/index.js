ecaee0b0314d884e58ba956ab33ef8531620ad10
EamonZhong <qddxzym@163.com>
2017-10-18 22:42:35 +0800

    扩展运算符

--- ECMAScript6/ES6/src/index.js
+++ ECMAScript6/ES6/src/index.js
@@ -1,2 +1,9 @@
-let a=1;
-console.log(a);
+//  扩展运算符
+// function kz(...arg){
+//   console.log(arg[0]);
+//   console.log(arg[1]);
+//   console.log(arg[2]);
+//   console.log(arg[3]);
+
+// }
+// kz(1,2,3);
